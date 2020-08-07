2acd7324a3245835d3bc93f5bf971dc043cc417e
Egor Bakanov <egorbakanov@gmail.com>
2020-08-07 15:16:41 +0700

    base player

---
+++ MJ60/Assets/_project/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public new Camera camera;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 10000000f))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
