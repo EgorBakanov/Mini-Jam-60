fa1df471b6061ba7937bea1bdaf5944aacd8a2b8
Egor Bakanov <egorbakanov@gmail.com>
2020-08-08 15:03:17 +0700

    add animation for activating pauser button

--- MJ60/Assets/_project/Scripts/SystemPauserUi.cs
+++ MJ60/Assets/_project/Scripts/SystemPauserUi.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,12 +11,17 @@ public class SystemPauserUi : MonoBehaviour, IPointerClickHandler
     public TMP_Text text;
     public Image fill;
     public Gradient fillGradient;
+    public RectTransform buttonWrapper;
+    public float openTime;
+    public Ease openEase;
 
     public void Init(PausableSystem system)
     {
         pausableSystem = system;
         text.text = pausableSystem.name;
         pausableSystem.OnValueChanged += OnValueChanged;
+        buttonWrapper.localScale = Vector3.zero;
+        buttonWrapper.DOScale(Vector3.one, openTime).SetEase(openEase);
     }
 
     private void OnDestroy()
