2827c7f49fbc753f141dd5a29fbf4db9a0c17e1b
Egor Bakanov <egorbakanov@gmail.com>
2020-08-09 18:29:47 +0700

    alarm ui

---
+++ MJ60/Assets/_project/Scripts/AlarmUi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlarmUi : MonoBehaviour
+{
+    public Slider slider;
+    public CanvasGroup wrapper;
+    public float noneScale = 1;
+    public float alarmScale = 1;
+    public float rescaleTime;
+    public Ease rescaleEase;
+    public float pauseAlpha;
+    
+    private AlarmSystem _alarmSystem;
+    
+    private void Start()
+    {
+        _alarmSystem = GameManager.Instance.pausableSystemManager.alarmSystem;
+        _alarmSystem.OnAlarmValueChanged += OnAlarmValueChanged;
+        _alarmSystem.OnPause += OnPause;
+        _alarmSystem.OnUnpause += OnUnpause;
+    }
+
+    private void OnUnpause()
+    {
+        wrapper.alpha = 1;
+    }
+
+    private void OnPause()
+    {
+        wrapper.alpha = pauseAlpha;
+    }
+
+    private void OnAlarmValueChanged(float value)
+    {
+        var old = slider.value;
+        slider.value = value;
+        if (value == 0f)
+            wrapper.transform.DOScale(noneScale, rescaleTime).SetEase(rescaleEase);
+        else if (value == 1f)
+            wrapper.transform.DOScale(alarmScale, rescaleTime).SetEase(rescaleEase);
+        else if(old == 0f || old == 1f)
+            wrapper.transform.DOScale(1f, rescaleTime).SetEase(rescaleEase);
+    }
+}
